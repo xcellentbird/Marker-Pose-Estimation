1a8cdafbc262c45c2e3b70df7b1562abbcf092ef
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2020-12-14 03:31:49 +0900

    Update tpublisher.cpp
    한글 주석 추가

--- topictest/src/tpublisher.cpp
+++ topictest/src/tpublisher.cpp
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 //#include <sstream>
 
+// 입력된 키보드의 char를 반환
 int getch(void)
 {
     struct termios oldattr, newattr;
@@ -33,6 +34,8 @@ int getche(void)
     tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
     return ch;
 }
+
+// 키보드 입력을 확인하는 함수
 bool kbhit()
 {
     termios term;
@@ -54,9 +57,9 @@ bool kbhit()
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "tpublisher"); 
-	ros::NodeHandle nh; 
-	ros::Publisher pub = nh.advertise<std_msgs::Bool>("Go_Cam",1);
-	ros::Rate loop_rate(10); 
+	ros::NodeHandle nh; // 노드 핸들러 선언 ( 참고로 노드 핸들러 여러 개를 선언할 수 있다 )
+	ros::Publisher pub = nh.advertise<std_msgs::Bool>("Go_Cam",1); // publiser 선언
+	ros::Rate loop_rate(10);
 	//std_msgs::String msg; 
 	std_msgs::Bool bool_msg;
 
@@ -74,25 +77,27 @@ int main(int argc, char** argv)
 	ROS_INFO("send camera set true");
 	while(true) 
 	{
+		// 키보드의 입력이 있을 경우
 		if(kbhit()){
+			// 입력을 char로 반환
 			c = getch();
 			switch(c)
 			{
-			case 'c':
+			case 'c': // 입력이 c인 경우 dat를 true로 설정
 				dat = true;
 				break;
-			default:
+			default: // 다른 입력의 경우 dat를 false로 설정
 				dat = false;
 				break;
 			}
 			//loop.sleep();
 		}
-		bool_msg.data = dat;
-		ROS_INFO("set %d",dat);
+		bool_msg.data = dat; // dat를 메세지 data형에 넣는다
+		ROS_INFO("set %d",dat); // printf와 같은 역할
 		//while(turn_while){
 			//int connections = pub.getNumSubscribers();
 			//if(connections > 0){
-				pub.publish(bool_msg);
+				pub.publish(bool_msg); // bool_msg를 publish
 
 				//turn_while = false;
 			//}
