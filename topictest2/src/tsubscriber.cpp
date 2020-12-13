211260ec6d58a0f92cf156d78dc07bcb0fa8b3be
xcellentbird <59414764+xcellentbird@users.noreply.github.com>
2020-12-14 03:34:34 +0900

    Update tsubscriber.cpp
    한글 주석 추가

--- topictest2/src/tsubscriber.cpp
+++ topictest2/src/tsubscriber.cpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/Pose2D.h"
 
 int cnt = 1;
+// Pose2D 메세지를 받는 콜백함수
 void msgCallBack(const geometry_msgs::Pose2D& msg)
 {
 	ROS_INFO("recieve %d th pose msg = x= %4.0f, z= %4.0f, p= %4.0f ",cnt,msg.x, msg.y, msg.theta);
@@ -10,11 +11,11 @@ void msgCallBack(const geometry_msgs::Pose2D& msg)
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "tsubscriber"); 
-	ros::NodeHandle nh; 
-	ros::Subscriber sub = nh.subscribe("Movement",1,msgCallBack);
+	ros::init(argc, argv, "tsubscriber"); // 노드 초기화
+	ros::NodeHandle nh; // 노드 핸들러 설정
+	ros::Subscriber sub = nh.subscribe("Movement",1,msgCallBack); // subscriber 설정 및 콜백 함수 설정
 	ROS_INFO("ready to receive Pose2D");
-	ros::spin();
+	ros::spin(); // spin - subscriber를 동작시키는 역할
 	return 0;
 }
 	
